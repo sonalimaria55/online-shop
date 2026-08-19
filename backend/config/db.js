aaace93a692fcadc03e6e674651b9098a90c1f00
sonalimaria55 <sonalimaria55@gmail.com>
2026-08-19 20:02:20 +0530

    db change

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,29 @@
+// const mongoose = require("mongoose");
 
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_URI);
+//     console.log(`MongoDB Connected`);
+//   } catch (error) {
+//     console.error("MongoDB Connection Error:");
+//     console.error(error.message);
+//   }
+// };
+
+// module.exports = connectDB;
+//-------------------------------------------
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected`);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
   } catch (error) {
-    console.error("MongoDB Connection Error:");
-    console.error(error.message);
+    console.error("MongoDB Connection Error:", error.message);
+    process.exit(1);
   }
 };
 
 module.exports = connectDB;
+
 
